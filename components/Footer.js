596d7b719a9ecf9733a61b893203dab664268442
doug4life <70630864+doug4life@users.noreply.github.com>
2022-02-11 12:38:32 -0500

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <>
       <footer className={styles.footer}>
-        Made with <img src="/netliheart.svg" alt="Netlify Logo" className={styles.logo} /> for you
+        Made with <img src="/Screenshot 2022-01-05 at 12.42.17 PM.png" alt="Netlify Logo" className={styles.logo} /> for you
       </footer>
     </>
   )
